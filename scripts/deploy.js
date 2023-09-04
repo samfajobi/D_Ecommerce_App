875d900f941fc0699ce3166ff3b8f24bf76b4824
Fajobi Olusegun <segzyfaj1@gmail.com>
2023-09-04 20:07:38 +0100

    Writing dep script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,16 @@
 
+import { ethers } from "hardhat";
+const hre = require("hardhat")
+
+async function main() {
+  const Transactions = await hre.ethers.getContractFactory("Transaction")
+  const transactions = await Transactions.deploy();
+
+
+  //await transactions.deployed();
+
+  
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
