9bc9d1b4586cd3405fc401834cb410c33b45f9ed
Fajobi Olusegun <segzyfaj1@gmail.com>
2023-09-04 20:06:35 +0100

    Writing deployment script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
-require("@nomicfoundation/hardhat-toolbox");
+//import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config();
+const {ALCHEMY_API_KEY, PRIVATE_KEY} = process.env;
+
 module.exports = {
-  solidity: "0.8.19",
-};
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [PRIVATE_KEY]
+    }
+  },
+  
+}
